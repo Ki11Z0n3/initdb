12bc7b12859049dabc73c9b55e1fdacfd9372d09
Javier Manga <jmangabloonde@gmail.com>
2019-12-16 17:11:39 +0100

    Upload

--- src/LaravelInitDBServiceProvider.php
+++ src/LaravelInitDBServiceProvider.php
@@ -2,25 +2,16 @@
 
 namespace JaviManga\InitDB;
 
-use App\Console\Commands\CloneDB;
-use App\Console\Commands\CreateDB;
-use App\Console\Commands\DropDB;
-use App\Console\Commands\InitDB;
+use JaviManga\InitDB\Console\Commands\CloneDB;
+use JaviManga\InitDB\Console\Commands\CreateDB;
+use JaviManga\InitDB\Console\Commands\DropDB;
+use JaviManga\InitDB\Console\Commands\InitDB;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelInitDBServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-//        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'httpResponses');
-//        $this->publishes([
-//            __DIR__ . '/../resources/lang' => resource_path('lang/en'),
-//        ], 'laravelabstractclass');
         if ($this->app->runningInConsole()) {
             $this->commands([
                 CLoneDB::class,
@@ -29,15 +20,5 @@ class LaravelInitDBServiceProvider extends ServiceProvider
                 InitDB::class,
             ]);
         }
-    }
-
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-//        include __DIR__ . '/routes/api.php';
     }
 }
