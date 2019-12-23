8a7ed4ecb470a0938511ea27eb2ffc2a38079fe5
Javier Manga <javier.manga@iagt.es>
2019-12-23 11:07:00 +0100

    Upload

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'JaviMaga\\InitDB\\' => array($vendorDir . '/javimanga/initdb/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,25 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd
 {
-    public static $prefixLengthsPsr4 = array (
+    public static $prefixesPsr0 = array (
         'J' => 
         array (
-            'JaviMaga\\InitDB\\' => 16,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'JaviMaga\\InitDB\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/javimanga/initdb/src',
+            'JaviManga/InitDB' => 
+            array (
+                0 => __DIR__ . '/../..' . '/src',
+            ),
         ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
