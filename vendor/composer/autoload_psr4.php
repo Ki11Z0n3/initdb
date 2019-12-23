e33342c254db2645ee822cec5e60ec303e303a21
Javier Manga <javier.manga@iagt.es>
2019-12-23 10:54:21 +0100

    Upload

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'JaviMaga\\InitDB\\' => array($vendorDir . '/javimanga/initdb/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd
 {
+    public static $prefixLengthsPsr4 = array (
+        'J' => 
+        array (
+            'JaviMaga\\InitDB\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'JaviMaga\\InitDB\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/javimanga/initdb/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit117f06d08f37bb6e0bd57cdaeb980bfd::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
